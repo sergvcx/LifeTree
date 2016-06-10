53932a65838452b23aef5d996de3bd1752ecb92e
sergvcx <sergvcx@mail.ru>
2016-06-10 13:26:15 +0300

    commit from home

--- main.cpp
+++ main.cpp
@@ -182,9 +182,39 @@ int life2xml(Task* pRootTask,char* fileName){
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
     //MainWindow w;
     //a.resize(10,10);
+    QMenuBar mnuBar;
+        QMenu*   pmnu   = new QMenu("&Menu");
+
+        pmnu->addAction("&About Qt",
+                        &app,
+                        SLOT(aboutQt()),
+                        Qt::CTRL + Qt::Key_Q
+                       );
+
+        pmnu->addSeparator();
+
+        QAction* pCheckableAction = pmnu->addAction("&CheckableItem");
+        pCheckableAction->setCheckable(true);
+        pCheckableAction->setChecked(true);
+
+        pmnu->addAction(QPixmap(img4_xpm), "&IconItem");
+
+        QMenu* pmnuSubMenu = new QMenu("&SubMenu", pmnu);
+        pmnu->addMenu(pmnuSubMenu);
+        pmnuSubMenu->addAction("&Test");
+
+        QAction* pDisabledAction = pmnu->addAction("&DisabledItem");
+        pDisabledAction->setEnabled(false);
+
+        pmnu->addSeparator();
+
+        pmnu->addAction("&Exit", &app, SLOT(quit()));
+
+        mnuBar.addMenu(pmnu);
+        mnuBar.show();
 
     Task  root("root");
     Task  life("life");
